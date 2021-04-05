29fc77feb2d0ecb84dfc00fe120ae7521fcb4b7f
Michael Vincent <mvincent1212@utexas.edu>
2021-04-04 20:36:26 -0500

    changed about.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,7 +4,7 @@ import { Container, Row, Col } from 'react-bootstrap';
 import Title from '../Title/Title';
 import AboutImg from '../Image/AboutImg';
 import PortfolioContext from '../../context/context';
-import resumes from "../../pdf/Michael_Vincent_Resume_Spring_2021.pdf"
+import resumes from "../../pdf/MichaelVincent_Resume"
 
 const About = () => {
   const { about } = useContext(PortfolioContext);
